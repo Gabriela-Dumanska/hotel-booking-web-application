2f5f9e770af177f543745e234548dc24af02f3d1
Gabriela Dumańska <dumanska@student.agh.edu.pl>
2023-11-01 22:43:31 +0100

    Potwierdzenie bookingu ready

--- app.js
+++ app.js
@@ -12,19 +12,36 @@ function getValues(){
     var adults= document.getElementById("nadults").value; 
     var kids= document.getElementById("nkids").value; 
     var type= document.getElementById("typ").value; 
-    //saving the values in local storage
     localStorage.setItem("val1", date1);
     localStorage.setItem("val2", date2);
     localStorage.setItem("val3", adults);
     localStorage.setItem("val4", kids); 
     localStorage.setItem("val5", type);
     window.location.href = 'booking.html';
-    }
-    
-    function myFunction(){
+}
+function getValues2(){
+        var name= document.getElementById("name").value;
+        var number=document.getElementById("number").value;
+        localStorage.setItem("name", name);
+        localStorage.setItem("number", number);
+        window.location.href = 'potwierdzenie.html';
+}
+function myFunction(){
     document.getElementById("data").innerHTML=localStorage.getItem("val1");
     document.getElementById("data1").innerHTML=localStorage.getItem("val2");
     document.getElementById("data2").innerHTML=localStorage.getItem("val3");
     document.getElementById("data3").innerHTML=localStorage.getItem("val4");
     document.getElementById("data4").innerHTML=localStorage.getItem("val5");
+    document.getElementById("name").innerHTML=localStorage.getItem("name");
+    document.getElementById("number").innerHTML=localStorage.getItem("number");
+    if (localStorage.getItem("val5")=="Standard"){
+        document.getElementById("roomphoto").src= "images/standardroom.jpg";
     }
+    if (localStorage.getItem("val5")=="Premium"){
+        document.getElementById("roomphoto").src= "images/premiumroom.jpg";
+    }
+    if (localStorage.getItem("val5")=="Vip"){
+        document.getElementById("roomphoto").src= "images/viproom.jpg";
+    }
+}
+
