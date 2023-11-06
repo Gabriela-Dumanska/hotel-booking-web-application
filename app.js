6c720b984d2e9720c3a9fcf86ff95b5e232e2d1a
Gabriela Dumańska <dumanska@student.agh.edu.pl>
2023-11-06 21:28:56 +0100

    READY WEBSITE

--- app.js
+++ app.js
@@ -1,39 +1,130 @@
 const menu=document.querySelector('#mobile-menu');
-const menuLinks=document.querySelector('.navbar__menu');
+const menuLinks=document.querySelector('.topBar__menu');
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
-function getValues(){
-    var date1 = document.getElementById("arrival").value;
-    var date2= document.getElementById("departure").value; 
-    var adults= document.getElementById("nadults").value; 
-    var kids= document.getElementById("nkids").value; 
-    var type= document.getElementById("typ").value; 
-    localStorage.setItem("val1", date1);
-    localStorage.setItem("val2", date2);
-    localStorage.setItem("val3", adults);
-    localStorage.setItem("val4", kids); 
-    localStorage.setItem("val5", type);
-    window.location.href = 'booking.html';
+let sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
+ 
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
 }
-function getValues2(){
-        var name= document.getElementById("name").value;
-        var number=document.getElementById("number").value;
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+ 
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+arrowLeft.addEventListener("click", function () {
+    if (current == 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+arrowRight.addEventListener("click", function () {
+    if (current == sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+ 
+startSlide();
+var counter=0;
+function easteregg(){
+    counter=counter+1;
+    if(counter>4){
+        window.location.href = 'https://media.tenor.com/TUmjIWdiLIcAAAAd/dog-door-dog-dance.gif';
+    }
+}
+function validateBookingForm(){
+    var arrival = document.getElementById("arrival").value;
+    var departure= document.getElementById("departure").value; 
+    var nadults= document.getElementById("nadults").value; 
+    var nkids= document.getElementById("nkids").value; 
+    var type= document.getElementById("roomtype").value; 
+    localStorage.setItem("val1", arrival);
+    localStorage.setItem("val2", departure);
+    localStorage.setItem("val3", nadults);
+    localStorage.setItem("val4", nkids); 
+    localStorage.setItem("val5", type);
+    var errors=[];
+    if (arrival>departure || arrival=='' || departure=='') {
+        errors.push('Błędny termin wyjazdu');
+    }
+    if (nadults<0 || nkids<0){
+        errors.push(' Liczba osób nie może być ujemna');
+    }
+    if (nadults=='' || nadults==0){
+        errors.push(' Błędna liczba dorosłych');
+    }
+    if(nadults>10 || nkids>10){
+        errors.push(' Niestety nie zapewnimy noclegu dla tylu osób');
+    }
+    
+    if(errors.length==0){
+        window.location.href = 'booking.html';
+    }
+    else{
+        errors.push(' Spróbuj ponownie :(');
+        alert(errors);
+    }
+
+}
+function getBookingValues2(){
+        const name= document.getElementById("bookingName").value;
+        const number=document.getElementById("bookingNumber").value;
         localStorage.setItem("name", name);
         localStorage.setItem("number", number);
-        window.location.href = 'potwierdzenie.html';
+        var errors=[];
+        const regn = /^[a-zA-Z \s ń ś ć ź ż ó ł ę ą ]{2,}$/g;
+        
+        if (!regn.test(name)) {
+            errors.push('Błędne imię');
+        }
+        const regnum = /^[0-9]{8,10}$/g;
+        if (!regnum.test(number)) {
+            errors.push(' Błędny numer telefonu');
+        }
+        if(errors.length==0){
+            window.location.href = 'potwierdzenie.html';
+        }
+        else{
+            errors.push(' Spróbuj ponownie :(');
+            alert(errors);
+        }
 }
-function myFunction(){
-    document.getElementById("data").innerHTML=localStorage.getItem("val1");
-    document.getElementById("data1").innerHTML=localStorage.getItem("val2");
-    document.getElementById("data2").innerHTML=localStorage.getItem("val3");
-    document.getElementById("data3").innerHTML=localStorage.getItem("val4");
-    document.getElementById("data4").innerHTML=localStorage.getItem("val5");
-    document.getElementById("name").innerHTML=localStorage.getItem("name");
-    document.getElementById("number").innerHTML=localStorage.getItem("number");
+function getBookingValues1(){
+    document.getElementById("bookingDataArrival").innerHTML=localStorage.getItem("val1");
+    document.getElementById("bookingDataDeparture").innerHTML=localStorage.getItem("val2");
+    document.getElementById("bookingDataNumAdults").innerHTML=localStorage.getItem("val3");
+    if(localStorage.getItem("val4")==''){
+        document.getElementById("bookingDataNumKids").innerHTML=0;
+    }
+    else{
+        document.getElementById("bookingDataNumKids").innerHTML=localStorage.getItem("val4");
+    }
+    document.getElementById("bookingDataRoomType").innerHTML=localStorage.getItem("val5");
+    document.getElementById("bookingName").innerHTML=localStorage.getItem("name");
+    document.getElementById("bookingNumber").innerHTML=localStorage.getItem("number");
     if (localStorage.getItem("val5")=="Standard"){
         document.getElementById("roomphoto").src= "images/standardroom.jpg";
     }
@@ -44,12 +135,46 @@ function myFunction(){
         document.getElementById("roomphoto").src= "images/viproom.jpg";
     }
 }
-function myFunction2(){
-    document.getElementById("data.2").innerHTML=localStorage.getItem("val1");
-    document.getElementById("data1.2").innerHTML=localStorage.getItem("val2");
-    document.getElementById("data2.2").innerHTML=localStorage.getItem("val3");
-    document.getElementById("data3.2").innerHTML=localStorage.getItem("val4");
-    document.getElementById("data4.2").innerHTML=localStorage.getItem("val5");
-    document.getElementById("name2").innerHTML=localStorage.getItem("name");
-    document.getElementById("number2").innerHTML=localStorage.getItem("number");
+function getConfirmationData(){
+    document.getElementById("bookingDataArrival").innerHTML=localStorage.getItem("val1");
+    document.getElementById("bookingDataDeparture").innerHTML=localStorage.getItem("val2");
+    document.getElementById("bookingDataNumAdults").innerHTML=localStorage.getItem("val3");
+    if(localStorage.getItem("val4")==''){
+        document.getElementById("bookingDataNumKids").innerHTML=0;
+    }
+    else{
+        document.getElementById("bookingDataNumKids").innerHTML=localStorage.getItem("val4");
+    }
+    document.getElementById("bookingDataRoomType").innerHTML=localStorage.getItem("val5");
+    document.getElementById("bookingDataName").innerHTML=localStorage.getItem("name");
+    document.getElementById("bookingDataNumber").innerHTML=localStorage.getItem("number");
 }
+function validateContactForm() {
+    var errors=[];
+    const name = document.forms["contactForm"]["fname"].value;
+    const email= document.forms["contactForm"]["femail"].value;
+    const mess= document.forms["contactForm"]["fmessage"].value;
+    const check= document.forms["contactForm"]["fcheckbox"];
+    const regn = /^[a-zA-Z \s ń ś ć ź ż ó ł ę ą ]{2,}$/g;
+    if (!regn.test(name)) {
+        errors.push('Błędne imię');
+    }
+    const regm= /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+    if (!regm.test(email)){
+        errors.push(' Błędny adres e-mail');
+    }
+    if(mess.length<1){
+        errors.push(' Za krótka wiadomość');
+    }
+    if(!check.checked){
+        errors.push(' Musisz zaakceptować nasz newsletter');
+    }
+    if(errors.length==0){
+        alert('Dziękujemy za twoją wiadomość! Do zobaczenia :)');
+    }
+    else{
+        errors.push(' Spróbuj ponownie :(');
+        alert(errors);
+    }
+}
+  
